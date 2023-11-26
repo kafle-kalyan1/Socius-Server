3ab1a4dae6b9d403c7a1bebdcbea235c238644f9
kalyan.kafle <kalyan.kafle@aegishms.com>
2023-11-26 18:44:39 +0545

    initial commit

--- server/settings.py
+++ server/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["socius.onrender.com", "localhost"]
 
 
 # Application definition
