22d012c13b46e032fd98f5fb4cc613d80caa0256
kafle-kalyan1 <kalyankafle123@gmail.com>
2024-02-10 21:22:39 +0545

    Add UserSettings model to UserUtils

--- UserUtils/models.py
+++ UserUtils/models.py
@@ -3,3 +3,13 @@ from django.db import models
 
 # Create your models here.
 
+class UserSettings(models.Model):
+   user = models.OneToOneField(User, on_delete=models.CASCADE)
+   dark_mode = models.BooleanField(default=False)
+   langauge = models.CharField(max_length=50, default="en")
+   message_notification = models.BooleanField(default=True)
+   friend_request_notification = models.BooleanField(default=True)
+   post_notification = models.BooleanField(default=True)
+   sync_post_for_offline = models.BooleanField(default=True)
+   
+   
