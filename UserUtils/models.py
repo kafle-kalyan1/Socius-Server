c4f1379a4285e0f869dbecbb3c85eccd773759ef
kafle-kalyan1 <kalyankafle123@gmail.com>
2024-02-28 23:50:17 +0545

    Add notification type choices to UserSettings model

--- UserUtils/models.py
+++ UserUtils/models.py
@@ -16,14 +16,24 @@ LANGUAGES = (
    ("np", "np"),
 )
 
+#choices for notification type
+NOTIFICATION_TYPE = (
+   ("push", "push"),
+   ("app", "app"),
+   ("both", "both"),
+   ("none", "none")
+)
+
 class UserSettings(models.Model):
    user = models.OneToOneField(User, on_delete=models.CASCADE)
    dark_mode = models.CharField(choices=DARK_MODE, default="auto", max_length=5)
    language = models.CharField(choices=LANGUAGES, default="en", max_length=2)
    
-   message_notification = models.BooleanField(default=True)
-   friend_request_notification = models.BooleanField(default=True)
-   post_notification = models.BooleanField(default=True)
+   message_notification = models.CharField(choices=NOTIFICATION_TYPE, default="app", max_length=5)
+   friend_request_notification = models.CharField(choices=NOTIFICATION_TYPE, default="app", max_length=5)
+   post_like_notification = models.CharField(choices=NOTIFICATION_TYPE, default="app", max_length=5)
+   post_comment_notification = models.CharField(choices=NOTIFICATION_TYPE, default="app", max_length=5)
+   request_accepted_notification = models.CharField(choices=NOTIFICATION_TYPE, default="app", max_length=5)
    
    sync_post_for_offline = models.BooleanField(default=True)
    
